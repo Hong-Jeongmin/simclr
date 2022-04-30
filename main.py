cb9dcc2c563ba69a40a22c5642d6364bb07d3961
Hong-Jeongmin <hzeongmin@gmail.com>
2022-05-01 00:48:09 +0900

    move dir

---
+++ main.py
@@ -0,0 +1,128 @@
+import os
+import argparse
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+import numpy as np
+import utils
+from model import Model
+from utils import EarlyStopping
+from tqdm import tqdm
+
+def train_val(net, data_loader, train_optimizer=None):
+    is_train = train_optimizer is not None
+    net.train() if is_train else net.eval()
+    
+    total_loss, total_num = 0.0, 0
+    data_bar = tqdm(data_loader, leave=False)
+    if is_train:
+        
+        for pos_1, pos_2, target in data_bar:
+            pos_1, pos_2 = pos_1.cuda(non_blocking=True), pos_2.cuda(non_blocking=True)
+            feature_1, out_1 = net(pos_1)
+            feature_2, out_2 = net(pos_2)
+            # [2*B, D]
+            out = torch.cat([out_1, out_2], dim=0)
+            # [2*B, 2*B]
+            sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
+
+            #mask
+            sim_matirx = sim_matrix * (torch.ones(2*batch_size, device=sim_matrix.device)
+                                       - torch.eye(2*batch_size,device=sim_matrix.device))
+
+            # compute loss
+            pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+            # [2*B]
+            pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
+            loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+
+            train_optimizer.zero_grad()
+            loss.backward()
+            train_optimizer.step()
+            
+            total_num += batch_size
+            total_loss += loss.item() * batch_size
+            data_bar.set_description('Epoch: [{}/{}] Loss: {:.4f}'.format(epoch, epochs, total_loss / total_num))
+            data_bar.set_description('Epoch: [{}/{}] Loss: {:.4f}'.format(epoch, epochs, loss.item()))
+            
+        
+            
+
+
+    else:
+        with torch.no_grad():
+            for pos_1, pos_2, target in data_bar:
+                pos_1, pos_2 = pos_1.cuda(non_blocking=True), pos_2.cuda(non_blocking=True)
+                feature_1, out_1 = net(pos_1)
+                feature_2, out_2 = net(pos_2)
+                # [2*B, D]
+                out = torch.cat([out_1, out_2], dim=0)
+                # [2*B, 2*B]
+                sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
+
+                #mask
+                sim_matirx = sim_matrix * (torch.ones(2*batch_size, device=sim_matrix.device)
+                                           - torch.eye(2*batch_size,device=sim_matrix.device))
+
+                # compute loss
+                pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+                # [2*B]
+                pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
+                loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+
+                total_num += batch_size
+                total_loss += loss.item() * batch_size
+                data_bar.set_description('Epoch: [{}/{}] Loss: {:.4f}'.format(epoch, epochs, loss.item()))
+
+    return total_loss/total_num
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train SimCLR batch_size=128')
+    parser.add_argument('--temperature', default=0.5, type=float, help='Temperature used in softmax')
+    parser.add_argument('--batch_size', default=512, type=int, help='Number of images in each mini-batch')
+    parser.add_argument('--epochs', default=500, type=int, help='Number of sweeps over the dataset to train')
+   
+
+    args = parser.parse_args()
+    batch_size, epochs = args.batch_size, args.epochs
+    temperature = args.temperature
+
+    train_data = utils.CIFAR10Pair(root='./data', train=True, transform=utils.train_transform, download=True)
+    train_loader, valid_loader = utils.create_datasets(batch_size, train_data)
+    # model setup and optimizer config
+
+
+    if not os.path.exists('results_batch{}_2'.format(batch_size)):
+        os.mkdir('results_batch{}_2'.format(batch_size))
+   
+    results = {'train_sim': [], 'valid_sim':[]}
+    
+    model = Model().cuda()
+    optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-6)
+    least_loss = np.Inf
+    
+    for epoch in range(1, epochs + 1):
+        train_loss = train_val(model, train_loader, optimizer)
+        valid_loss = train_val(model, valid_loader)
+        
+        results['train_sim'].append(train_loss)
+        results['valid_sim'].append(valid_loss)
+        
+        # save statistics
+        data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
+        data_frame.to_csv(f'results_batch{batch_size}_2/statistics.csv', index_label='epoch')
+    '''    
+        if valid_loss < least_loss:
+            least_loss = valid_loss
+            best_epoch = epoch
+            torch.save(model.state_dict(), f'results_batch{batch_size}_2/model.pth')
+       
+    print("Best epoch: ", best_epoch)
+    '''
+                
---
+++ utils.py
@@ -0,0 +1,115 @@
+import numpy as np
+import torch
+from PIL import Image
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+from torch.utils.data.sampler import SubsetRandomSampler
+
+
+def create_datasets(batch_size, train_data):
+    # trainning set 중 validation 데이터로 사용할 비율
+    valid_size = 0.2
+
+    # validation으로 사용할 trainning indices를 얻는다.
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    np.random.shuffle(indices)
+    split = int(np.floor(valid_size * num_train))
+    train_idx, valid_idx = indices[split:], indices[:split]
+
+    # trainning, validation batch를 얻기 위한 sampler정의
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    # load training data in batches
+    train_loader = torch.utils.data.DataLoader(train_data,
+                                               batch_size=batch_size,
+                                               sampler=train_sampler,
+                                               num_workers=12, drop_last=True)
+
+    # load validation data in batches
+    valid_loader = torch.utils.data.DataLoader(train_data,
+                                               batch_size=batch_size,
+                                               sampler=valid_sampler,
+                                               num_workers=12, drop_last=True)
+
+    return train_loader, valid_loader
+
+class CIFAR10Pair(CIFAR10):
+    """CIFAR10 Dataset."""
+
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            pos_1 = self.transform(img)
+            pos_2 = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return pos_1, pos_2, target
+    
+    
+
+train_transform = transforms.Compose([
+    transforms.RandomResizedCrop(32),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+    transforms.RandomGrayscale(p=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
+
+    
+class EarlyStopping:
+    """주어진 patience 이후로 validation loss가 개선되지 않으면 학습을 조기 중지"""
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt'):
+        """
+        Args:
+            patience (int): validation loss가 개선된 후 기다리는 기간
+                            Default: 7
+            verbose (bool): True일 경우 각 validation loss의 개선 사항 메세지 출력
+                            Default: False
+            delta (float): 개선되었다고 인정되는 monitered quantity의 최소 변화
+                            Default: 0
+            path (str): checkpoint저장 경로
+                            Default: 'checkpoint.pt'
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''validation loss가 감소하면 모델을 저장한다.'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
+        
